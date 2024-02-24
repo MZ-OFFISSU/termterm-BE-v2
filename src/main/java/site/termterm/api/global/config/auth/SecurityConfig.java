d4f558d72549fb5c589f0bd85b6a5e6f7aed028e
Seungwoo, Joo <thisis.joos@gmail.com>
2024-02-24 13:57:53 +0900

    chore(config): 주석 수정

--- src/main/java/site/termterm/api/global/config/auth/SecurityConfig.java
+++ src/main/java/site/termterm/api/global/config/auth/SecurityConfig.java
@@ -38,29 +38,29 @@ public class SecurityConfig {
         }
     }
 
+    // JWT 인증 방식을 사용할 예정이다. Session 을 사용하지 않는다.
     @Bean
-    // JWT 인증 방식을 사용할 예정이다. Session을 사용하지 않는다.
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
-        httpSecurity.headers().frameOptions().disable();    // iframe을 허용하지 않는다
+        httpSecurity.headers().frameOptions().disable();    // iframe 을 허용하지 않는다
 
-        httpSecurity.csrf().disable();      // csrf가 enable이면 postman이 작동하지 않는다.
-        httpSecurity.cors().configurationSource(null);      // 일단 null이라고 해놓는다. 나중에 설정할 예정
+        httpSecurity.csrf().disable();                      // csrf 가 enable 이면 postman 이 작동하지 않는다.
+        httpSecurity.cors().configurationSource(null);      // 일단 null 이라고 해놓는다. 나중에 설정할 예정
 
-        // jsessionId를 서버쪽에서 관리하지 않겠다
+        // jsessionId 를 서버쪽에서 관리하지 않겠다
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // /login 경로의 폼 로그인을 받지 않겠다
         httpSecurity.formLogin().disable();
 
-        // httpBasic은 브라우저 팝업창을 이용해서 사용자 인증을 진행한다.
+        // httpBasic 은 브라우저 팝업창을 이용해서 사용자 인증을 진행한다.
         httpSecurity.httpBasic().disable();
 
         // 필터 적용
         httpSecurity.apply(new CustomSecurityFilterManager());
 
         // Exception 가로채기
-        /* 인증 or 권한 관련된 오류가 나면, spring security는 필터에서 가로채서 처리를 한다. 이 제어권을 우리가 뺏어온다 */
+        /* 인증 or 권한 관련된 오류가 나면, spring security 는 필터에서 가로채서 처리를 한다. 이 제어권을 우리가 뺏어온다 */
         // 1. 인증실패
         httpSecurity.exceptionHandling().authenticationEntryPoint(((request, response, authException) -> {
             CustomResponseUtil.fail(response, "로그인을 해 주세요.", HttpStatus.UNAUTHORIZED);
@@ -83,8 +83,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");        // GET, POST, PUT, DELETE  (Javascript 요청 허용)
-        configuration.addAllowedOriginPattern("*");     // 모든 IP 주소 허용  (나중에 프론트엔드 IP만 허용)  + 앱은 모든 사용자들이 다른 IP를 쓰기 때문에 어떻게 해줄 수가 없다. CORS에 걸리지도 않는다.
-        configuration.setAllowCredentials(true);        // Client에서 Cookie 요청 허용
+        configuration.addAllowedOriginPattern("*");     // 모든 IP 주소 허용  (나중에 프론트엔드 IP만 허용)  + 앱은 모든 사용자들이 다른 IP를 쓰기 때문에 어떻게 해줄 수가 없다. CORS 에 걸리지도 않는다.
+        configuration.setAllowCredentials(true);        // Client 에서 Cookie 요청 허용
         configuration.setExposedHeaders(List.of("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
