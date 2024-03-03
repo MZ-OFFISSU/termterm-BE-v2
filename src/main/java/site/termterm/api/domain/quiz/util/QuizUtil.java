fa306e079e06501bad9c60bc696424a86bb1826c
Seungwoo, Joo <thisis.joos@gmail.com>
2024-03-03 13:50:14 +0900

    feat(quiz): 매일 자정 daily quiz 데이터 자동 삭제
    - @Scheduled cron 으로 구현

---
+++ src/main/java/site/termterm/api/domain/quiz/util/QuizUtil.java
@@ -0,0 +1,27 @@
+package site.termterm.api.domain.quiz.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import site.termterm.api.domain.member.repository.MemberRepository;
+import site.termterm.api.domain.quiz.entity.QuizStatus;
+import site.termterm.api.domain.quiz.repository.QuizRepository;
+
+@Component
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class QuizUtil {
+    private final QuizRepository quizRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 매일 0시 퀴즈 데이터 삭제
+     */
+    @Transactional
+    @Scheduled(cron = "${scheduled.cron.quiz.initialize}") // 매일 0시
+    public void deleteQuizData(){
+        quizRepository.deleteAll();
+        memberRepository.findAll().forEach(member -> member.setQuizStatus(QuizStatus.NOT_STARTED));
+    }
+}
