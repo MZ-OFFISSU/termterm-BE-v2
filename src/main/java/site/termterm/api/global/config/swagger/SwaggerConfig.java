52b7caa4d137ed57da2f3dab879fa6bd9f231273
Seungwoo, Joo <thisis.joos@gmail.com>
2024-03-09 22:01:14 +0900

    fix(swagger): swagger 요청 url 에서 https 가능케 변경

--- src/main/java/site/termterm/api/global/config/swagger/SwaggerConfig.java
+++ src/main/java/site/termterm/api/global/config/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package site.termterm.api.global.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -12,8 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 
 @OpenAPIDefinition(
-        info = @Info(title = "termterm api server v2",
-                version = "v2"))
+        info = @Info(title = "termterm api server v2", version = "v2"),
+        servers = {@Server(url = "/", description = "Default Server URL")})
 @Configuration
 public class SwaggerConfig {
 
